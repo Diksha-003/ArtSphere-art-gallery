79af2bf93cce041454d66da5e327934251729d50
Diksha-003 <ddhar.987@gmail.com>
2024-03-06 15:13:46 +0530

    .Net

---
+++ Project/Controllers/ArtistImagesController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.Models;
+using newUser.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+[Authorize(Roles = "Admin,User,Artist")]
+public class ArtistImagesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ArtistImagesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var artistImages = await _context.ArtistImages.ToListAsync();
+        return View(artistImages);
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Admin")] // Only allow Admin role to delete images
+    public IActionResult DeleteImage(int imageId)
+    {
+        var image = _context.ArtistImages.Find(imageId);
+
+        if (image == null)
+        {
+            return NotFound(); // Image not found
+        }
+
+        _context.ArtistImages.Remove(image);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [Authorize(Roles = "Artist")]
+    public async Task<IActionResult> UserImages()
+    {
+        var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (string.IsNullOrEmpty(currentUserId) || !User.IsInRole("Artist"))
+        {
+            return Forbid(); // User is not authorized
+        }
+
+        var userImages = await _context.ArtistImages
+            .Where(img => img.ArtistId == currentUserId)
+            .ToListAsync();
+
+        return View(userImages);
+    }
+
+    [Authorize(Roles = "Artist")]
+    public async Task<IActionResult> EditUserImage(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound(); // If id is null, return Not Found
+        }
+
+        var image = await _context.ArtistImages.FindAsync(id);
+
+        if (image == null)
+        {
+            return NotFound(); // If image with given id is not found, return Not Found
+        }
+
+        return View(image);
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Artist")]
+    public async Task<IActionResult> EditUserImage(int id, [Bind("Id,ImageDescription,ImageFilePath")] ArtistImage artistImage)
+    {
+        if(id != artistImage.Id)
+        {
+            return NotFound();
+        }
+        //var image = await _context.ArtistImages.FindAsync(id);
+
+        //if (image == null)
+        //{
+            //return NotFound(); // Image not found
+        //}
+
+        //var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+       // if (!User.IsInRole("Admin") && image.ArtistId != currentUserId)
+        //{
+            //return Forbid(); // User is not authorized to edit this image
+       // }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                //image.ImageDescription = artistImage.ImageDescription;
+                //image.ImageFilePath = artistImage.ImageFilePath;
+
+                _context.Update(artistImage);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ImageExists(artistImage.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                } // Image not found
+            }
+
+            return RedirectToAction(nameof(UserImages));
+        }
+
+        return View(artistImage);
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Artist")]
+    public IActionResult DeleteUserImage(int imageId)
+    {
+        var image = _context.ArtistImages.Find(imageId);
+
+        if (image == null)
+        {
+            return NotFound(); // Image not found
+        }
+
+        var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (!User.IsInRole("Admin") && image.ArtistId != currentUserId)
+        {
+            return Forbid(); // User is not authorized to delete this image
+        }
+
+        _context.ArtistImages.Remove(image);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(UserImages));
+    }
+
+    private bool ImageExists(int imageId)
+    {
+        return _context.ArtistImages.Any(e => e.Id == imageId);
+    }
+}
---
+++ Project/Controllers/ExhbController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.Models;
+using newUser.Data;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using System.Linq;
+
+[Authorize(Roles = "Admin,User,Artist")]
+public class ExhbController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ExhbController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var exhibitions = await _context.exhb.ToListAsync();
+        return View(exhibitions);
+    }
+
+    // GET: Exhb/Edit/5
+    [Authorize(Roles = "Admin")] // Only allow Admin role to edit exhibitions
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound(); // If id is null, return Not Found
+        }
+
+        var exhibition = await _context.exhb.FindAsync(id);
+
+        if (exhibition == null)
+        {
+            return NotFound(); // If exhibition with given id is not found, return Not Found
+        }
+
+        return View(exhibition);
+    }
+
+    // POST: Exhb/Edit/5
+    [HttpPost]
+    
+    [Authorize(Roles = "Admin")] // Only allow Admin role to edit exhibitions
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Venue,ImageFilePath")] Exhb exhb)
+    {
+        if (id != exhb.Id)
+        {
+            return NotFound(); // If id in URL doesn't match id in the posted model, return Not Found
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(exhb);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExhibitionExists(exhb.Id))
+                {
+                    return NotFound(); // If exhibition doesn't exist, return Not Found
+                }
+                else
+                {
+                    throw; // Throw exception if there's a concurrency issue
+                }
+            }
+            return RedirectToAction(nameof(Index)); // Redirect to index action after successful edit
+        }
+        return View(exhb);
+    }
+
+    // POST: Exhb/Delete/5
+    [HttpPost, ActionName("DeleteExhibition")]
+    
+    [Authorize(Roles = "Admin")] // Only allow Admin role to delete exhibitions
+    public IActionResult DeleteExhibition(int exhibitionId)
+    {
+        var exhibition = _context.exhb.Find(exhibitionId);
+
+        if (exhibition == null)
+        {
+            return NotFound(); // Exhibition not found
+        }
+
+        _context.exhb.Remove(exhibition);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool ExhibitionExists(int exhibitionId)
+    {
+        return _context.exhb.Any(e => e.Id == exhibitionId);
+    }
+}
